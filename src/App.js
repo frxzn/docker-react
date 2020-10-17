096a4ac50013e7ebe30cde158491764f89fd8e6f
martin <tin.maldonado@gmail.com>
2020-10-17 01:14:57 -0300

    new feature

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          New Feature Branch
         </a>
       </header>
     </div>
